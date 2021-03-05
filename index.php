ff8b13e4e8b15b1075f737ef0a614160d1ce384e
frostkom <martin.fors@frostkom.se>
2021-03-05 12:39:44 +0100

    Make sure that MF Base is active before doing anything

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF STR Webshop
 Plugin URI: https://github.com/frostkom/mf_str_webshop
 Description: Wordpress plugin that adds support for an STR webshop
-Version: 1.4.3
+Version: 1.4.4
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
@@ -14,34 +14,37 @@ Depends: MF Base
 GitHub Plugin URI: frostkom/mf_str_webshop
 */
 
-include_once("include/classes.php");
-
-load_plugin_textdomain('lang_str_webshop', false, dirname(plugin_basename(__FILE__)).'/lang/');
-
-$obj_str_webshop = new mf_str_webshop();
-
-add_action('cron_base', array($obj_str_webshop, 'cron_base'), mt_rand(1, 10));
-
-if(is_admin())
+if(is_plugin_active("mf_base/index.php"))
 {
-	register_uninstall_hook(__FILE__, 'uninstall_str_webshop');
+	include_once("include/classes.php");
 
-	add_action('admin_init', array($obj_str_webshop, 'settings_str_webshop'));
-	add_action('admin_menu', array($obj_str_webshop, 'admin_menu'));
-}
+	load_plugin_textdomain('lang_str_webshop', false, dirname(plugin_basename(__FILE__)).'/lang/');
 
-else
-{
-	add_action('wp_head', array($obj_str_webshop, 'wp_head'), 0);
-}
+	$obj_str_webshop = new mf_str_webshop();
 
-add_shortcode('mf_str_webshop', array($obj_str_webshop, 'shortcode_str_webshop'));
+	add_action('cron_base', array($obj_str_webshop, 'cron_base'), mt_rand(1, 10));
 
-add_filter('recommend_config', array($obj_str_webshop, 'recommend_config'));
+	if(is_admin())
+	{
+		register_uninstall_hook(__FILE__, 'uninstall_str_webshop');
 
-function uninstall_str_webshop()
-{
-	mf_uninstall_plugin(array(
-		'options' => array('setting_str_webshop_post_id', 'setting_str_webshop_api_mode', 'setting_str_webshop_customer_number', 'setting_str_webshop_google_analytics', 'setting_str_webshop_include_css', 'setting_str_webshop_include_extra_css', 'setting_str_webshop_header_selector'),
-	));
+		add_action('admin_init', array($obj_str_webshop, 'settings_str_webshop'));
+		add_action('admin_menu', array($obj_str_webshop, 'admin_menu'));
+	}
+
+	else
+	{
+		add_action('wp_head', array($obj_str_webshop, 'wp_head'), 0);
+	}
+
+	add_shortcode('mf_str_webshop', array($obj_str_webshop, 'shortcode_str_webshop'));
+
+	add_filter('recommend_config', array($obj_str_webshop, 'recommend_config'));
+
+	function uninstall_str_webshop()
+	{
+		mf_uninstall_plugin(array(
+			'options' => array('setting_str_webshop_post_id', 'setting_str_webshop_api_mode', 'setting_str_webshop_customer_number', 'setting_str_webshop_google_analytics', 'setting_str_webshop_include_css', 'setting_str_webshop_include_extra_css', 'setting_str_webshop_header_selector'),
+		));
+	}
 }
