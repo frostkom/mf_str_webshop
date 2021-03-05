021ba46521bab10ff1657ee821b9e34fbb45f50c
frostkom <martin.fors@frostkom.se>
2021-03-05 10:29:29 +0100

    Make it possible to change URL to make it easier to send instructions to someone ellse

--- manual/index.php
+++ manual/index.php
@@ -2,6 +2,16 @@
 
 $obj_str_webshop = new mf_str_webshop();
 
+$site_url = get_site_url();
+$strSiteUrl = check_var('strSiteUrl', 'char', true, $site_url);
+
+$obj_str_webshop->admin_url = str_replace($site_url, $strSiteUrl, $site_url."/wp-admin/");
+$obj_str_webshop->plugins_url = str_replace($site_url, $strSiteUrl, admin_url("plugins.php"));
+$obj_str_webshop->pages_url = str_replace($site_url, $strSiteUrl, admin_url("edit.php?post_type=page"));
+$obj_str_webshop->settings_base_url = str_replace($site_url, $strSiteUrl, admin_url("options-general.php?page=settings_mf_base"));
+$obj_str_webshop->settings_webshop_url = str_replace($site_url, $strSiteUrl, admin_url("options-general.php?page=settings_mf_base#settings_str_webshop"));
+$obj_str_webshop->settings_github_url = str_replace($site_url, $strSiteUrl, $obj_str_webshop->github_settings_url);
+
 echo "<div class='wrap'>
 	<h2>".__("Manual", $obj_str_webshop->lang_key)."</h2>
 	<div id='poststuff'>
@@ -14,8 +24,8 @@ echo "<div class='wrap'>
 							<ol>
 								<li>".sprintf(__("Download ZIP files from %s", $obj_str_webshop->lang_key), "<a href='//drive.google.com/drive/folders/1Z7dtsUwxUMJW_Cf5AN6FR2G78Qqqi-e1'>Drive</a>")."</li>
 								<li>".sprintf(__("Upload ZIP files to %s", $obj_str_webshop->lang_key), "<code>wp-content/plugins</code>")."</li>
-								<li>".sprintf(__("Login to %s", $obj_str_webshop->lang_key), "<a href='".wp_login_url()."'>Wordpress</a>")."</li>
-								<li>".sprintf(__("Go to %sPlugins%s and activate %s first, then %s and %s", $obj_str_webshop->lang_key), "<a href='".admin_url("plugins.php")."'>", "</a>", "MF Base", "MF STR Webshop", "GitHub Updater")."</li>
+								<li>".sprintf(__("Login to %s", $obj_str_webshop->lang_key), "<a href='".$obj_str_webshop->admin_url."'>Wordpress</a>")."</li>
+								<li>".sprintf(__("Go to %sPlugins%s and activate %s first, then %s and %s", $obj_str_webshop->lang_key), "<a href='".$obj_str_webshop->plugins_url."'>", "</a>", "MF Base", "MF STR Webshop", "GitHub Updater")."</li>
 							</ol>
 						</div>
 					</div>
@@ -23,12 +33,13 @@ echo "<div class='wrap'>
 						<h3 class='hndle'><span>".__("Activate webshop on a page", $obj_str_webshop->lang_key)."</span></h3>
 						<div class='inside'>
 							<ol>
-								<li>".sprintf(__("Open %sPages%s and edit the page you plan to have the webshop on", $obj_str_webshop->lang_key), "<a href='".admin_url("edit.php?post_type=page")."'>", "</a>")."</li>
+								<li>".sprintf(__("Open %sPages%s and edit the page you plan to have the webshop on", $obj_str_webshop->lang_key), "<a href='".$obj_str_webshop->pages_url."'>", "</a>")."</li>
 								<li>".sprintf(__("Add the shortcode %s where you want the webshop to appear on the page", $obj_str_webshop->lang_key), "<code>[mf_str_webshop]</code>")."</li>
-								<li>".sprintf(__("Go to Settings -> My Settings -> %sCommon%s", $obj_str_webshop->lang_key), "<a href='".admin_url("options-general.php?page=settings_mf_base")."'>", "</a>")."</li>
+								<li>".sprintf(__("Go to Settings -> My Settings -> %sCommon%s", $obj_str_webshop->lang_key), "<a href='".$obj_str_webshop->settings_base_url."'>", "</a>")."</li>
 								<li>".sprintf(__("Choose Yes on Automatically Update %s and save", $obj_str_webshop->lang_key), ".htaccess")."</li>
-								<li>".sprintf(__("Go to Settings -> My Settings -> %sWebshop%s", $obj_str_webshop->lang_key), "<a href='".admin_url("options-general.php?page=settings_mf_base#settings_str_webshop")."'>", "</a>")."</li>
+								<li>".sprintf(__("Go to Settings -> My Settings -> %sWebshop%s", $obj_str_webshop->lang_key), "<a href='".$obj_str_webshop->settings_webshop_url."'>", "</a>")."</li>
 								<li>".__("Choose the page that you previously added the shortcode to, choose Test/Live, add the Customer Number and save", $obj_str_webshop->lang_key)."</li>
+								<li>".sprintf(__("If the Cachier button is not visible in the Cart, go to Settings -> My Settings -> %sWebshop%s and set Include Extra CSS to Yes", $obj_str_webshop->lang_key), "<a href='".$obj_str_webshop->settings_webshop_url."'>", "</a>")."</li>
 								<li>".__("Clear the server cache if there is a Cache plugin installed", $obj_str_webshop->lang_key)."</li>
 							</ol>
 						</div>
@@ -37,7 +48,7 @@ echo "<div class='wrap'>
 						<h3 class='hndle'><span>".sprintf(__("Activate automatic updates of plugins on %s", $obj_str_webshop->lang_key), "GitHub")."</span></h3>
 						<div class='inside'>
 							<ol>
-								<li>".sprintf(__("Go to Settings -> %s -> %s", $obj_str_webshop->lang_key), "GitHub Updater", "<a href='".$obj_str_webshop->github_settings_url."'>GitHub</a>")."</li>
+								<li>".sprintf(__("Go to Settings -> %s -> %s", $obj_str_webshop->lang_key), "GitHub Updater", "<a href='".$obj_str_webshop->settings_github_url."'>GitHub</a>")."</li>
 								<li>";
 
 									$github_updater = get_site_option('github_updater');
@@ -53,11 +64,19 @@ echo "<div class='wrap'>
 									}
 									
 								echo "</li>
+								<li>".sprintf(__("If the tab %s and field %s is not immediately visible, be patient and go back in a few minutes to check", $obj_str_webshop->lang_key), "GitHub", "GitHub.com Access Token")."</li>
 							</ol>
 						</div>
 					</div>
 				</div>
 				<div id='postbox-container-1'>
+					<div class='postbox'>
+						<h3 class='hndle'><span>".__("Send Instruction", $obj_str_webshop->lang_key)."</span></h3>
+						<div class='inside'>"
+							.show_textfield(array('type' => 'url', 'name' => 'strSiteUrl', 'value' => $strSiteUrl, 'description' => __("If you want to send any of the instructions below to someone else, add their site URL here to change all links below", $obj_str_webshop->lang_key)))
+							.show_button(array('name' => 'btnWebshopReload', 'text' => __("Reload", $obj_str_webshop->lang_key)))
+						."</div>
+					</div>
 					<div class='postbox'>
 						<h3 class='hndle'><span>".__("Status", $obj_str_webshop->lang_key)."</span></h3>
 						<div class='inside'>".$obj_str_webshop->get_status(array('type' => 'html'))."</div>
