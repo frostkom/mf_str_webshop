b67b9703e3a2fede5fdfcb856473bbac1bbbd17d
frostkom <martin.fors@frostkom.se>
2025-12-18 10:28:21 +0100

    Standardize localization

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: MF STR Webshop
 Plugin URI: https://github.com/frostkom/mf_str_webshop
-Description: Wordpress plugin that adds support for an STR webshop
-Version: 1.7.26
+Description: Adds support for an STR webshop
+Version: 1.7.27
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
